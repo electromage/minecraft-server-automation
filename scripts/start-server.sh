678bfbbedaa9cdf13fee76737960aedd1f109c37
electromage <electromage@gmail.com>
2024-05-25 14:24:14 -0700

    start-server

---
+++ scripts/start-server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case $1 in
+  start)
+    /usr/bin/tmux new-session -d -s minecraft '/usr/bin/java -Xmx1024M -Xms1024M -jar server.jar nogui'
+    ;;
+  stop)
+    if /usr/bin/tmux has-session -t minecraft 2>/dev/null; then
+      /usr/bin/tmux send-keys -t minecraft 'say SERVER SHUTTING DOWN. Saving map...' Enter 'save-all' Enter 'stop' Enter
+    fi
+    ;;
+esac
